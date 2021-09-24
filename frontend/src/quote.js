fe127e4c0718eebf38f11a26676949142fc4283b
JackQuinoa <jquinoa208@gmail.com>
2021-09-24 17:20:42 -0400

    made quoteHTML function

--- frontend/src/quote.js
+++ frontend/src/quote.js
@@ -1,15 +1,28 @@
 class Quote {
 
     static all = []
+    static quoteContainer = document.getElementById("quotes-container")
 
     constructor(id, phrase, stoic_id){
         this.id = id
         this.phrase = phrase
         this.stoic_id = stoic_id
 
+        this.element = document.createElement('li')
+        this.element.dataset.id = this.id 
+        this.element.id = `quote-${this.id}`
 
         Quote.all.push(this)
     }
 
+    quoteHTML(){
+        this.element.innerHTML += `
+            <div>
+                <h4>${this.phrase}</h4>
+            </div>
+
+        `
+        return this.element
+    }
 
 }
