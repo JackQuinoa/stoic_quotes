17333f196276f133f394d419415a554bc5dcec4e
JackQuinoa <jquinoa208@gmail.com>
2021-09-24 10:52:18 -0400

    added fetch for quotes

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,2 @@
-const base_url = "http://127.0.0.1:3000/"
+const base_url = "http://127.0.0.1:3000"
 const quoteService = new QuoteService(base_url)
--- frontend/src/quoteService.js
+++ frontend/src/quoteService.js
@@ -3,4 +3,14 @@ class QuoteService{
     constructor(endpoint){
         this.endpoint = endpoint
     }
+
+    // Read/Index Action
+
+    getQuotes(){
+        fetch(`${this.endpoint}/quotes`)
+
+    }
+
+
+
 }
